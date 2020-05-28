d22105f2415227e264c43d1019b501d28f2913b2
dmitryweiner <dmitry.weiner@gmail.com>
2020-05-28 18:06:15 +0700

    Updates

---
+++ precache-manifest.ea20b9bbc93dc78920c2a028937423a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9d289276615a35914e54ac8bdb55168",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "d98ab12f68d514123254",
+    "url": "/react-memory-game/static/css/main.b450ba11.chunk.css"
+  },
+  {
+    "revision": "fd02adea1f50ffedab8e",
+    "url": "/react-memory-game/static/js/2.811bb958.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-memory-game/static/js/2.811bb958.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d98ab12f68d514123254",
+    "url": "/react-memory-game/static/js/main.0c0aff2b.chunk.js"
+  },
+  {
+    "revision": "eb79215dff71741c0973",
+    "url": "/react-memory-game/static/js/runtime-main.9e2c059b.js"
+  }
+]);
